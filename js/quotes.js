f4df9a213d176543d638f300d09a7699145faeca
Steven Abaco <stevenabaco@gmail.com>
2021-03-30 11:10:11 -0500

    Add Quotes

---
+++ js/quotes.js
@@ -0,0 +1,29 @@
+/**  
+* Create an array of quote objects to be used for quoteGenerator
+* Each quote should be an object with the following properties:
+* quote - a string containing the text of the quote that will be displayed on the page.
+* source - a string containing the creator of the quote. For example: "Mark Twain" or "Traditional Irish proverb” or "Anonymous".
+* citation - optional - a string identifying where the quote comes from, like a speech, publication or a movie. If there is no known publication, do not include this property on the object.
+* year - optional - a number identifying the year of the quote. If there is no known year, then do not include this property on the object.
+**/
+
+let quotes = [
+	{
+		quote: "If you want to live a happy life, tie it to a goal, not to people or things",
+		source: "Albert Einstein",
+		citation: "",
+		year: "",
+	},
+	{
+		quote: "Stay away from negative people. They have a problem for every solution",
+		source: "Albert Einstein",
+		citation: "",
+		year: "",
+	},
+	{
+		quote: "Success is the Ability to go from one failure to another with no loss of enthusiasm",
+		source: "Winston Churchill",
+		citation: "",
+		year: "",
+	},
+];
