d83a6c2bd534ddfc3407602e4df24037b328513a
Steven Abaco <stevenabaco@gmail.com>
2021-03-30 12:13:01 -0500

    Update files and structure with starting project files

---
+++ js/script.js
@@ -0,0 +1,50 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array 
+***/
+let quotes = [
+	{
+		quote: "If you want to live a happy life, tie it to a goal, not to people or things",
+		source: "Albert Einstein",
+		citation: "",
+		year: "",
+	},
+	{
+		quote: "Stay away from negative people. They have a problem for every solution",
+		source: "Albert Einstein",
+		citation: "",
+		year: "",
+	},
+	{
+		quote: "Success is the Ability to go from one failure to another with no loss of enthusiasm",
+		source: "Winston Churchill",
+		citation: "",
+		year: "",
+	},
+];
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
